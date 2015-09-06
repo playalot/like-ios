166813ba4b7bec381ca5c48ab652fb3f91518537
黄伟锋 <vinson@likeorz.com>
2015-09-06 11:24:40 +0800

    Vinson

---
+++ LIKE/LIKE/LKExternal/LKNetwork/LKBatchRequest.m
@@ -0,0 +1,158 @@
+//
+//  LKBatchRequest.m
+//
+//  Copyright (c) 2012-2014 LKNetwork https://github.com/yuantiku
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+#import "LKBatchRequest.h"
+#import "LKNetworkPrivate.h"
+#import "LKBatchRequestAgent.h"
+
+@interface LKBatchRequest() <LKRequestDelegate>
+
+@property (nonatomic) NSInteger finishedCount;
+
+@end
+
+@implementation LKBatchRequest
+
+- (id)initWithRequestArray:(NSArray *)requestArray {
+    self = [super init];
+    if (self) {
+        _requestArray = [requestArray copy];
+        _finishedCount = 0;
+        for (LKRequest * req in _requestArray) {
+            if (![req isKindOfClass:[LKRequest class]]) {
+                LKLog(@"Error, request item must be LKRequest instance.");
+                return nil;
+            }
+        }
+    }
+    return self;
+}
+
+- (void)start {
+    if (_finishedCount > 0) {
+        LKLog(@"Error! Batch request has already started.");
+        return;
+    }
+    [[LKBatchRequestAgent sharedInstance] addBatchRequest:self];
+    [self toggleAccessoriesWillStartCallBack];
+    for (LKRequest * req in _requestArray) {
+        req.delegate = self;
+        [req start];
+    }
+}
+
+- (void)stop {
+    [self toggleAccessoriesWillStopCallBack];
+    _delegate = nil;
+    [self clearRequest];
+    [self toggleAccessoriesDidStopCallBack];
+    [[LKBatchRequestAgent sharedInstance] removeBatchRequest:self];
+}
+
+- (void)startWithCompletionBlockWithSuccess:(void (^)(LKBatchRequest *batchRequest))success
+                                    failure:(void (^)(LKBatchRequest *batchRequest))failure {
+    [self setCompletionBlockWithSuccess:success failure:failure];
+    [self start];
+}
+
+- (void)setCompletionBlockWithSuccess:(void (^)(LKBatchRequest *batchRequest))success
+                              failure:(void (^)(LKBatchRequest *batchRequest))failure {
+    self.successCompletionBlock = success;
+    self.failureCompletionBlock = failure;
+}
+
+- (void)clearCompletionBlock {
+    // nil out to break the retain cycle.
+    self.successCompletionBlock = nil;
+    self.failureCompletionBlock = nil;
+}
+
+- (BOOL)isDataFromCache {
+    BOOL result = YES;
+    for (LKRequest *request in _requestArray) {
+        if (!request.isDataFromCache) {
+            result = NO;
+        }
+    }
+    return result;
+}
+
+
+- (void)dealloc {
+    [self clearRequest];
+}
+
+#pragma mark - Network Request Delegate
+
+- (void)requestFinished:(LKRequest *)request {
+    _finishedCount++;
+    if (_finishedCount == _requestArray.count) {
+        [self toggleAccessoriesWillStopCallBack];
+        if ([_delegate respondsToSelector:@selector(batchRequestFinished:)]) {
+            [_delegate batchRequestFinished:self];
+        }
+        if (_successCompletionBlock) {
+            _successCompletionBlock(self);
+        }
+        [self clearCompletionBlock];
+        [self toggleAccessoriesDidStopCallBack];
+    }
+}
+
+- (void)requestFailed:(LKRequest *)request {
+    [self toggleAccessoriesWillStopCallBack];
+    // Stop
+    for (LKRequest *req in _requestArray) {
+        [req stop];
+    }
+    // Callback
+    if ([_delegate respondsToSelector:@selector(batchRequestFailed:)]) {
+        [_delegate batchRequestFailed:self];
+    }
+    if (_failureCompletionBlock) {
+        _failureCompletionBlock(self);
+    }
+    // Clear
+    [self clearCompletionBlock];
+    
+    [self toggleAccessoriesDidStopCallBack];
+    [[LKBatchRequestAgent sharedInstance] removeBatchRequest:self];
+}
+
+- (void)clearRequest {
+    for (LKRequest * req in _requestArray) {
+        [req stop];
+    }
+    [self clearCompletionBlock];
+}
+
+#pragma mark - Request Accessoies
+
+- (void)addAccessory:(id<LKRequestAccessory>)accessory {
+    if (!self.requestAccessories) {
+        self.requestAccessories = [NSMutableArray array];
+    }
+    [self.requestAccessories addObject:accessory];
+}
+
+@end
---
+++ LIKE/LIKE/LKExternal/LKNetwork/LKNetworkAgent.m
@@ -0,0 +1,285 @@
+//
+//  LKNetworkAgent.m
+//
+//  Copyright (c) 2012-2014 LKNetwork https://github.com/yuantiku
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+#import "LKNetworkAgent.h"
+#import "LKNetworkConfig.h"
+#import "LKNetworkPrivate.h"
+#import "AFDownloadRequestOperation.h"
+
+@implementation LKNetworkAgent {
+    AFHTTPRequestOperationManager *_manager;
+    LKNetworkConfig *_config;
+    NSMutableDictionary *_requestsRecord;
+}
+
++ (LKNetworkAgent *)sharedInstance {
+    static id sharedInstance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [[self alloc] init];
+    });
+    return sharedInstance;
+}
+
+- (id)init {
+    self = [super init];
+    if (self) {
+        _config = [LKNetworkConfig sharedInstance];
+        _manager = [AFHTTPRequestOperationManager manager];
+        _requestsRecord = [NSMutableDictionary dictionary];
+        _manager.operationQueue.maxConcurrentOperationCount = 4;
+    }
+    return self;
+}
+
+- (NSString *)buildRequestUrl:(LKBaseRequest *)request {
+    NSString *detailUrl = [request requestUrl];
+    if ([detailUrl hasPrefix:@"http"]) {
+        return detailUrl;
+    }
+    // filter url
+    NSArray *filters = [_config urlFilters];
+    for (id<LKUrlFilterProtocol> f in filters) {
+        detailUrl = [f filterUrl:detailUrl withRequest:request];
+    }
+
+    NSString *baseUrl;
+    if ([request useCDN]) {
+        if ([request cdnUrl].length > 0) {
+            baseUrl = [request cdnUrl];
+        } else {
+            baseUrl = [_config cdnUrl];
+        }
+    } else {
+        if ([request baseUrl].length > 0) {
+            baseUrl = [request baseUrl];
+        } else {
+            baseUrl = [_config baseUrl];
+        }
+    }
+    return [NSString stringWithFormat:@"%@%@", baseUrl, detailUrl];
+}
+
+- (void)addRequest:(LKBaseRequest *)request {
+    LKRequestMethod method = [request requestMethod];
+    NSString *url = [self buildRequestUrl:request];
+    id param = request.requestArgument;
+    AFConstructingBlock constructingBlock = [request constructingBodyBlock];
+
+    if (request.requestSerializerType == LKRequestSerializerTypeHTTP) {
+        _manager.requestSerializer = [AFHTTPRequestSerializer serializer];
+    } else if (request.requestSerializerType == LKRequestSerializerTypeJSON) {
+        _manager.requestSerializer = [AFJSONRequestSerializer serializer];
+    }
+    
+    _manager.requestSerializer.timeoutInterval = [request requestTimeoutInterval];
+
+    // if api need server username and password
+    NSArray *authorizationHeaderFieldArray = [request requestAuthorizationHeaderFieldArray];
+    if (authorizationHeaderFieldArray != nil) {
+        [_manager.requestSerializer setAuthorizationHeaderFieldWithUsername:(NSString *)authorizationHeaderFieldArray.firstObject
+                                                                   password:(NSString *)authorizationHeaderFieldArray.lastObject];
+    }
+    
+    // if api need add custom value to HTTPHeaderField
+    NSDictionary *headerFieldValueDictionary = [request requestHeaderFieldValueDictionary];
+    if (headerFieldValueDictionary != nil) {
+        for (id httpHeaderField in headerFieldValueDictionary.allKeys) {
+            id value = headerFieldValueDictionary[httpHeaderField];
+            if ([httpHeaderField isKindOfClass:[NSString class]] && [value isKindOfClass:[NSString class]]) {
+                [_manager.requestSerializer setValue:(NSString *)value forHTTPHeaderField:(NSString *)httpHeaderField];
+            } else {
+                LKLog(@"Error, class of key/value in headerFieldValueDictionary should be NSString.");
+            }
+        }
+    }
+
+    // if api build custom url request
+    NSURLRequest *customUrlRequest= [request buildCustomUrlRequest];
+    if (customUrlRequest) {
+        AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:customUrlRequest];
+        [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+            [self handleRequestResult:operation];
+        } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+            [self handleRequestResult:operation];
+        }];
+        request.requestOperation = operation;
+        operation.responseSerializer = _manager.responseSerializer;
+        [_manager.operationQueue addOperation:operation];
+    } else {
+        if (method == LKRequestMethodGet) {
+            if (request.resumableDownloadPath) {
+                // add parameters to URL;
+                NSString *filteredUrl = [LKNetworkPrivate urlStringWithOriginUrlString:url appendParameters:param];
+
+                NSURLRequest *requestUrl = [NSURLRequest requestWithURL:[NSURL URLWithString:filteredUrl]];
+                AFDownloadRequestOperation *operation = [[AFDownloadRequestOperation alloc] initWithRequest:requestUrl
+                                                                                                 targetPath:request.resumableDownloadPath shouldResume:YES];
+                [operation setProgressiveDownloadProgressBlock:request.resumableDownloadProgressBlock];
+                [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+                    [self handleRequestResult:operation];
+                }                                failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+                    [self handleRequestResult:operation];
+                }];
+                request.requestOperation = operation;
+                [_manager.operationQueue addOperation:operation];
+            } else {
+                request.requestOperation = [_manager GET:url parameters:param success:^(AFHTTPRequestOperation *operation, id responseObject) {
+                    [self handleRequestResult:operation];
+                }                                failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+                    [self handleRequestResult:operation];
+                }];
+            }
+        } else if (method == LKRequestMethodPost) {
+            if (constructingBlock != nil) {
+                request.requestOperation = [_manager POST:url parameters:param constructingBodyWithBlock:constructingBlock
+                                                  success:^(AFHTTPRequestOperation *operation, id responseObject) {
+                                                      [self handleRequestResult:operation];
+                                                  } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+                            [self handleRequestResult:operation];
+                        }];
+            } else {
+                request.requestOperation = [_manager POST:url parameters:param success:^(AFHTTPRequestOperation *operation, id responseObject) {
+                    [self handleRequestResult:operation];
+                }                                 failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+                    [self handleRequestResult:operation];
+                }];
+            }
+        } else if (method == LKRequestMethodHead) {
+            request.requestOperation = [_manager HEAD:url parameters:param success:^(AFHTTPRequestOperation *operation) {
+                [self handleRequestResult:operation];
+            }                                 failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+                [self handleRequestResult:operation];
+            }];
+        } else if (method == LKRequestMethodPut) {
+            request.requestOperation = [_manager PUT:url parameters:param success:^(AFHTTPRequestOperation *operation, id responseObject) {
+                [self handleRequestResult:operation];
+            }                                failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+                [self handleRequestResult:operation];
+            }];
+        } else if (method == LKRequestMethodDelete) {
+            request.requestOperation = [_manager DELETE:url parameters:param success:^(AFHTTPRequestOperation *operation, id responseObject) {
+                [self handleRequestResult:operation];
+            }                                   failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+                [self handleRequestResult:operation];
+            }];
+        } else if (method == LKRequestMethodPatch) {
+            request.requestOperation = [_manager PATCH:url parameters:param success:^(AFHTTPRequestOperation *operation, id responseObject) {
+                [self handleRequestResult:operation];
+            } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+                [self handleRequestResult:operation];
+            }];
+        } else {
+            LKLog(@"Error, unsupport method type");
+            return;
+        }
+    }
+
+    LKLog(@"Add request: %@", NSStringFromClass([request class]));
+    [self addOperation:request];
+}
+
+- (void)cancelRequest:(LKBaseRequest *)request {
+    [request.requestOperation cancel];
+    [self removeOperation:request.requestOperation];
+    [request clearCompletionBlock];
+}
+
+- (void)cancelAllRequests {
+    NSDictionary *copyRecord = [_requestsRecord copy];
+    for (NSString *key in copyRecord) {
+        LKBaseRequest *request = copyRecord[key];
+        [request stop];
+    }
+}
+
+- (BOOL)checkResult:(LKBaseRequest *)request {
+    BOOL result = [request statusCodeValidator];
+    if (!result) {
+        return result;
+    }
+    id validator = [request jsonValidator];
+    if (validator != nil) {
+        id json = [request responseJSONObject];
+        result = [LKNetworkPrivate checkJson:json withValidator:validator];
+    }
+    return result;
+}
+
+- (void)handleRequestResult:(AFHTTPRequestOperation *)operation {
+    NSString *key = [self requestHashKey:operation];
+    LKBaseRequest *request = _requestsRecord[key];
+    LKLog(@"Finished Request: %@", NSStringFromClass([request class]));
+    if (request) {
+        BOOL succeed = [self checkResult:request];
+        if (succeed) {
+            [request toggleAccessoriesWillStopCallBack];
+            [request requestCompleteFilter];
+            if (request.delegate != nil) {
+                [request.delegate requestFinished:request];
+            }
+            if (request.successCompletionBlock) {
+                request.successCompletionBlock(request);
+            }
+            [request toggleAccessoriesDidStopCallBack];
+        } else {
+            LKLog(@"Request %@ failed, status code = %ld",
+                     NSStringFromClass([request class]), (long)request.responseStatusCode);
+            [request toggleAccessoriesWillStopCallBack];
+            [request requestFailedFilter];
+            if (request.delegate != nil) {
+                [request.delegate requestFailed:request];
+            }
+            if (request.failureCompletionBlock) {
+                request.failureCompletionBlock(request);
+            }
+            [request toggleAccessoriesDidStopCallBack];
+        }
+    }
+    [self removeOperation:operation];
+    [request clearCompletionBlock];
+}
+
+- (NSString *)requestHashKey:(AFHTTPRequestOperation *)operation {
+    NSString *key = [NSString stringWithFormat:@"%lu", (unsigned long)[operation hash]];
+    return key;
+}
+
+- (void)addOperation:(LKBaseRequest *)request {
+    if (request.requestOperation != nil) {
+        NSString *key = [self requestHashKey:request.requestOperation];
+        @synchronized(self) {
+            _requestsRecord[key] = request;
+        }
+    }
+}
+
+- (void)removeOperation:(AFHTTPRequestOperation *)operation {
+    NSString *key = [self requestHashKey:operation];
+    @synchronized(self) {
+        [_requestsRecord removeObjectForKey:key];
+    }
+    LKLog(@"Request queue size = %lu", (unsigned long)[_requestsRecord count]);
+}
+
+@end

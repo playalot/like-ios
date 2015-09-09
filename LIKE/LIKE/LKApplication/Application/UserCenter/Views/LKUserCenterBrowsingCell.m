716a9ff0c816327ce6c72b2fbb40a4dc7afcae78
黄伟锋 <vinson@likeorz.com>
2015-09-09 14:05:11 +0800

    some feature for browsing

---
+++ LIKE/LIKE/LKApplication/Application/UserCenter/Views/LKUserCenterBrowsingCell.m
@@ -0,0 +1,21 @@
+//
+//  LKUserCenterBrowsingCell.m
+//  LIKE
+//
+//  Created by huangweifeng on 9/6/15.
+//  Copyright (c) 2015 Beijing Like Technology Co.Ltd . ( http://www.likeorz.com ). All rights reserved.
+//
+
+#import "LKUserCenterBrowsingCell.h"
+
+@implementation LKUserCenterBrowsingCell
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end

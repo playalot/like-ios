0444a95fe6bec3d93b44e9b3e9a45358db9d62d5
黄伟锋 <vinson@likeorz.com>
2015-09-29 21:04:43 +0800

    HomeFeedViewController update tags synchronizedly with detail viewcontroller

--- LIKE/LIKE/LKApplication/Common/LKPostTableViewController.m
+++ LIKE/LIKE/LKApplication/Common/LKPostTableViewController.m
@@ -11,12 +11,15 @@
 #import "LKTagAddModel.h"
 #import "LKPostDetailViewController.h"
 #import "LKLoginViewController.h"
+#import "LKInputView.h"
+#import "LCUIKeyBoard.h"
 
 static NSString* const KVO_CONTEXT_DATASOURCE_CHANGED = @"KVO_CONTEXT_DATASOURCE_CHANGED";
 
 @interface LKPostTableViewController () <LKPostDetailViewControllerDelegate>
 
-LC_PROPERTY(strong) LCUIPullLoader * pullLoader;
+LC_PROPERTY(strong) LKInputView *inputView;
+LC_PROPERTY(strong) LCUIPullLoader *pullLoader;
 
 LC_PROPERTY(assign) id observedDataSourceObject;
 LC_PROPERTY(copy) NSString *observedDataSourceKeyPath;
@@ -31,16 +34,59 @@ LC_PROPERTY(copy) NSString *observedDataSourceKeyPath;
     [self.observedDataSourceObject removeObserver:self forKeyPath:self.observedDataSourceKeyPath];
 }
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
+- (void)buildInputView {
     
-    if (self.datasource == nil)
-        self.datasource = [NSMutableArray array];
-    [self reloadData];
+    @weakly(self);
+    
+    self.inputView = LKInputView.view;
+    self.inputView.viewFrameY = self.view.viewFrameHeight;
+    self.view.ADD(self.inputView);
+    
+    self.inputView.sendAction = ^(NSString * string){
+        
+        @normally(self);
+        
+        if (string.trim.length == 0) {
+            [self showTopMessageErrorHud:LC_LO(@"标签不能为空")];
+            return;
+        }
+        
+        if (string.length > 12) {
+            [self showTopMessageErrorHud:LC_LO(@"标签长度不能大于12位")];
+            return;
+        }
+        
+        LKPostTableViewCell *cell = (LKPostTableViewCell *)[self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:self.inputView.tag inSection:0]];
+        
+        // 调加标签接口
+        if ([self checkTag:string onTags:((LKPost *)self.inputView.userInfo).tags]) {
+            [self addTag:string onPost:self.inputView.userInfo onCell:cell];
+        } else {
+            [self.view showTopMessageErrorHud:LC_LO(@"该标签已存在")];
+            [self.inputView resignFirstResponder];
+            self.inputView.textField.text = @"";
+        }
+    };
+    
+    self.inputView.didShow = ^(){
+        
+        @normally(self);
+        LKPostTableViewCell * cell = (LKPostTableViewCell *)[self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:self.inputView.tag inSection:0]];
+        CGFloat height1 = LC_DEVICE_HEIGHT - cell.viewFrameHeight;
+        CGFloat height2 = LCUIKeyBoard.singleton.height + self.inputView.viewFrameHeight - height1;
+        [self.tableView setContentOffset:LC_POINT(0, cell.viewFrameY + height2 - 25 + 10 + 63) animated:YES];
+    };
+    
+    self.inputView.willDismiss = ^(id value){
+    };
 }
 
 - (void)buildUI {
     self.view.backgroundColor = LKColor.backgroundColor;
+    
+    if (self.datasource == nil)
+        self.datasource = [NSMutableArray array];
+    [self reloadData];
     
     [self setNavigationBarButton:LCUINavigationBarButtonTypeLeft image:[UIImage imageNamed:@"NavigationBarBack.png" useCache:YES] selectImage:nil];
     [self.navigationController.navigationBar setBackgroundImage:[UIImage imageWithColor:LKColor.color andSize:CGSizeMake(LC_DEVICE_WIDTH, 64)] forBarMetrics:UIBarMetricsDefault];
@@ -65,6 +111,8 @@ LC_PROPERTY(copy) NSString *observedDataSourceKeyPath;
     };
     
     [self.tableView scrollToRowAtIndexPath:[NSIndexPath indexPathForItem:self.currentIndex inSection:0] atScrollPosition:UITableViewScrollPositionTop animated:NO];
+    
+    [self buildInputView];
 }
 
 - (void)setCurrentIndex:(NSInteger)currentIndex {
@@ -119,7 +167,16 @@ LC_PROPERTY(copy) NSString *observedDataSourceKeyPath;
 
 #pragma mark -
 
--(void) addTag:(NSString *)tag onPost:(LKPost *)post onCell:(LKPostTableViewCell *)cell {
+- (BOOL)checkTag:(NSString *)tag onTags:(NSArray *)onTags {
+    for (LKTag * oTag in onTags) {
+        if ([oTag.tag isEqualToString:tag]) {
+            return NO;
+        }
+    }
+    return YES;
+}
+
+- (void)addTag:(NSString *)tag onPost:(LKPost *)post onCell:(LKPostTableViewCell *)cell {
     @weakly(self);
     LKTag * tagValue = [[LKTag alloc] init];
     tagValue.tag = tag;
@@ -167,7 +224,7 @@ LC_PROPERTY(copy) NSString *observedDataSourceKeyPath;
 
 #pragma mark - ***** 数据源方法 *****
 
--(NSInteger) numberOfSectionsInTableView:(UITableView *)tableView {
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     return 1;
 }
 
@@ -186,16 +243,12 @@ LC_PROPERTY(copy) NSString *observedDataSourceKeyPath;
     @weakly(self);
     
     cell.addTag = ^(LKPost * value){
-        
-//    @normally(self);
-        
-//        if(![LKLoginViewController needLoginOnViewController:[LCUIApplication sharedInstance].window.rootViewController]){
-//            
-//            self.inputView.userInfo = value;
-//            self.inputView.tag = indexPath.row;
-//            
-//            [self.inputView becomeFirstResponder];
-//        }
+        @normally(self);
+        if(![LKLoginViewController needLoginOnViewController:[LCUIApplication sharedInstance].window.rootViewController]){
+            self.inputView.userInfo = value;
+            self.inputView.tag = indexPath.row;
+            [self.inputView becomeFirstResponder];
+        }
     };
     
     cell.removedTag = ^(LKPost * value){
